39a64c3657bffe5e1ae109441581e7933fce9b4d
umeshgupta1 <umesh_admin@AGI-LT-M079.netdecisions.local>
2021-12-30 16:26:45 +0530

    header & filter sticky

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,18 @@
 jQuery(document).ready(function($) {
+  if ($(window).scrollTop() > 40) {
+    $('.header-main').addClass('sticky');
+  }
+  else {
+    $('.header-main').removeClass('sticky');
+  }
+  $(window).bind('scroll', function() {
+    if ($(window).scrollTop() > 40) {
+      $('.header-main').addClass('sticky');
+    }
+    else {
+      $('.header-main').removeClass('sticky');
+    }
+  });
   $(".filter-box").each(function() {
     var $this = $(this).find(".filter-title");
     $this.click(function() {
