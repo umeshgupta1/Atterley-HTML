a8fb70170dfd5e3cad85e0df1c6ef58697e97e96
umeshgupta1 <umesh_admin@AGI-LT-M079.netdecisions.local>
2022-01-03 18:16:39 +0530

    mobile responsive

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,13 @@
 jQuery(document).ready(function($) {
   if ($(window).scrollTop() > 40) {
     $('.header-main').addClass('sticky');
-  }
-  else {
+  } else {
     $('.header-main').removeClass('sticky');
   }
   $(window).bind('scroll', function() {
     if ($(window).scrollTop() > 40) {
       $('.header-main').addClass('sticky');
-    }
-    else {
+    } else {
       $('.header-main').removeClass('sticky');
     }
   });
@@ -29,4 +27,50 @@ jQuery(document).ready(function($) {
       $(".filter-box").removeClass("active");
     }
   });
+
+  function mobAction() {
+    $(window).on("resize", function() {
+      if ($(window).width() < 767) {
+        $(".menu-btn").click(function(e) {
+          $("body").addClass("menu-open");
+          $(".cat-menu").fadeIn(150).animate({ left: "0" }, 150);
+        });
+        $(".close-menu-btn").click(function(e) {
+          $("body").removeClass("menu-open");
+          $(".cat-menu").animate({ left: "-100%" }, 150).fadeOut(150);
+        });
+        $("nav>ul>li>a").click(function(e) {
+          e.preventDefault();
+          $("nav>ul>li").removeClass("active");
+          $(this).parent("li").addClass("active");
+        });
+        $(".nav-div li.parent a").click(function(e) {
+          e.preventDefault();
+          $(this).toggleClass("active");
+        });
+        $(".shop-by-category .submenu-wrap").each(function() {
+          var $this = $(this);
+          var $thisul = $this.find("ul:first-child");
+          $this.find(".last").prependTo($thisul);
+        });
+        $(".mobile-filter").click(function(e) {
+          $(".filter-wrapper .filter-inner").fadeIn(150).animate({ left: "0" }, 150);
+          $("body").addClass("menu-open");
+        });
+        $(".filter-close-btn").click(function(e) {
+          $("body").removeClass("menu-open");
+          $(".filter-wrapper .filter-inner").animate({ left: "-100%" }, 150).fadeOut(150);
+        });
+        $(".mob-acco .row-heading").click(function() {
+          $(this).toggleClass("active");
+        });
+      } else {
+        $("body").removeClass("menu-open");
+        $(".cat-menu").attr("style", "");
+        $(".filter-wrapper .filter-inner").attr("style", "");
+        $(".mob-acco .row-heading").removeClass("active");
+      }
+    }).resize();
+  }
+  mobAction();
 });
